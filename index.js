8af3e330b9295841e4c6e288b72a3ea70e9aff01
m00447540 <mochalov.ilia@huawei.com>
2018-06-29 17:17:52 +0800

    Basic buttons and countdown

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const {app, BrowserWindow} = require('electron')
 
 function CreateWindow() {
     // main window
-    win = new BrowserWindow({width: 400, height: 400})
+    win = new BrowserWindow({width: 600, height: 400})
     
     // load index.html
     win.loadFile('index.html')
+
+    // XXX for development
+    win.toggleDevTools()
 }
 
 app.on('ready', CreateWindow)
